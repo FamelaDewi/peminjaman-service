f766b2a3fe07684502e5b9f25296fff30675cab5
FamelaDewi <113493540+FamelaDewi@users.noreply.github.com>
2023-01-04 18:31:10 +0700

    Add files via upload

---
+++ peminjaman-service/src/main/java/com/famela/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.famela.peminjaman.service.VO;
+
+import com.famela.peminjaman.service.entity.Peminjaman;
+
+/**
+ *
+ * @author HP
+ */
+public class ResponseTemplateVO {
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAnggota(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
